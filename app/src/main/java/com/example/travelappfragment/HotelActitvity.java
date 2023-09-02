1a4f03e952139ec7d1aeb1c606183ee43d6e174b
Khiem <2051052062khiem@ou.edu.vn>
2023-09-02 17:52:55 +0700

    them new saturday

--- app/src/main/java/com/example/travelappfragment/HotelActitvity.java
+++ app/src/main/java/com/example/travelappfragment/HotelActitvity.java
@@ -1,10 +1,14 @@
 package com.example.travelappfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -59,10 +63,17 @@ public class HotelActitvity extends AppCompatActivity implements PopupMenu.OnMen
             finish();
             return true;
         } else if(menuItem.getItemId() == R.id.review){
-            return true;
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.hotel_act, new ReviewFragment());
+            fragmentTransaction.commit();
         } else if(menuItem.getItemId() == R.id.favorite){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.hotel_act, new FavoriteFragment());
+            fragmentTransaction.commit();
             return true;
         }else if(menuItem.getItemId() == R.id.account){
+            Intent intent = new Intent(HotelActitvity.this, EditProfile.class);
+            startActivity(intent);
             return true;
         }
         return true;
